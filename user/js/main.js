dd39dbe1d3ba39c0afd6d39b41beb7b69b8b62bd
septimusforster <septimusforster@gmail.com>
2024-05-18 23:24:29 +0100

    User

---
+++ user/js/main.js
@@ -0,0 +1,12 @@
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCnGk02gQeUZ9nJeOBxHMk3jlC2_pG_jZo",
+    authDomain: "flutterspace-d2385.firebaseapp.com",
+    projectId: "flutterspace-d2385",
+    storageBucket: "flutterspace-d2385.appspot.com",
+    messagingSenderId: "979544012314",
+    appId: "1:979544012314:web:c2eef86fccbae61f17c3a3",
+    measurementId: "G-5E3NVV96HY"
+  };
